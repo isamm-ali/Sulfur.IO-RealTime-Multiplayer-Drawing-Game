f05cf4803eebc3bd55f135fb633004eed5ab355e
Isam Ali <isamali.work@gmail.com>
2026-09-13 01:27:55 +0530

    removed extra import

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/home_screen.dart';
-import 'package:frontend/screens/paint_screen.dart';
 
 void main() {
   runApp(const MyApp());
